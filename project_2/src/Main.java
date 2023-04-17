5ba1e39961240e1a552134bcb7e512b22a66d4e4
Eduardo Ramos <up201906732@fe.up.pt>
2023-04-17 14:57:34 +0100

    Its starting to wok but still meh

--- project_2/src/Main.java
+++ project_2/src/Main.java
@@ -1,5 +1,29 @@
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Game_Server server = new Game_Server();
+        Client client1 = new Client();
+        Client client2 = new Client();
+
+        Thread t = new Thread(()-> {
+            try {
+                server.start(2);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        t.start();
+
+        Thread t2 = new Thread(()-> {
+            try {
+                client1.start();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        t2.start();
+        client2.start();
+
     }
 }
